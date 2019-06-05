3a845b56897cfb1123693736dd0633da2611d4c8
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-06-05 19:23:17 -0400

    modify after search view style

--- zip-search/src/components/DisplayResults.js
+++ zip-search/src/components/DisplayResults.js
@@ -4,14 +4,18 @@ class DisplayResults extends Component {
     render() {
         return (
             <div>
-                <label>
+                <ul class="a">
+                 <label class="header">
                 <p>{this.props.result.city + ", " + this.props.result.state}</p>
                 </label> <br/>  
-                <label><p2 id="info">State: {this.props.result.state}<br/>
-                    Location: ({this.props.result.lat + " " + this.props.result.long}) <br/>
-                    Population (estimated): {this.props.result.population} <br/>
-                    Total Pages: {this.props.result.wages}
-                     </p2></label>
+                <label>
+                    <li>State: {this.props.result.state}</li>
+                   <li> Location: ({this.props.result.lat + " " + this.props.result.long}) </li>
+                    <li>Population (estimated): {this.props.result.population}</li>
+                   <li> Total Wages: {this.props.result.wages}</li>
+            </label>
+            </ul> 
+                
             </div>
         );
     }
